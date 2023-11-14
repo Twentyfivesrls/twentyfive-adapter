e703abe48b5332f78705e39e8959c36a1e40373c
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-11-14 11:34:15 +0100

    add event id in ticket

--- src/main/java/twentyfive/twentyfiveadapter/adapter/Document/TicketObjDocumentDB/TicketDocumentDB.java
+++ src/main/java/twentyfive/twentyfiveadapter/adapter/Document/TicketObjDocumentDB/TicketDocumentDB.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 public class TicketDocumentDB {
     @Id
     private String id;
+    private String eventId;
     private String eventName;
     private String code;
     private LocalDateTime eventDateStart;
