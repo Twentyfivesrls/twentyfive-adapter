6b75dfd094c09ea4770a8bf608adae1d73c12e4e
Simone Bria <simone.bria@iphonedeveloper.it>
2024-01-29 14:56:08 +0100

    updated version for package - twentyfiveapps dipendente fix

---
+++ src/main/java/twentyfive/twentyfiveadapter/adapter/Document/TwentyfiveAppsDocumentDB/TwentyfiveAppDocumentDB.java
@@ -0,0 +1,22 @@
+package twentyfive.twentyfiveadapter.adapter.Document.TwentyfiveAppsDocumentDB;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "twentyfive_apps")
+public class TwentyfiveAppDocumentDB {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private Byte[] imageContent;
+    private Boolean isPremium;
+    private Boolean isIncoming;
+}
