2a4a7cb664d8ffaf0779cc3c46cbd38a127768c7
twentyfivesrl <aurelius99w@hotmail.it>
2024-04-19 11:13:18 +0200

    commit di aurelio, models ecommerce generic, unique variables v0.0.82

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/BundleInPurchase.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/BundleInPurchase.java
@@ -3,6 +3,7 @@ package twentyfive.twentyfiveadapter.generic.ecommerce.models.dinamic;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import twentyfive.twentyfiveadapter.generic.ecommerce.utils.Measure;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
 public class BundleInPurchase {
     private String bundleId; // L'ID del bundle
     private int quantity; // La quantità di bundle
+    private Measure measure;
     private double totalPrice;
     private List<String> productWeightedIds;
 }
