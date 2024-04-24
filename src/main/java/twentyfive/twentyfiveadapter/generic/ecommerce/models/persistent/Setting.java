d4281a0e19f708e0c1ad7e418bfa8d4055659203
Raikozu <francescogaglianese93@gmail.com>
2024-04-24 11:48:07 +0200

    -Modified Setting and TimeSlot

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/persistent/Setting.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/persistent/Setting.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import twentyfive.twentyfiveadapter.generic.ecommerce.utils.DateRange;
 import twentyfive.twentyfiveadapter.generic.ecommerce.utils.WeightRange;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Data
@@ -15,7 +16,7 @@ public class Setting {
     private String id;
     private DateRange businessHours; // Orari di apertura collegati alla lavorazione del prodotto
     private DateRange labHours;
-    private Map<WeightRange, Integer> preparationTimeRanges; // Mappa che collega range di peso ai tempi di preparazione
+    private Map<WeightRange, LocalDateTime> preparationTimeRanges; // Mappa che collega range di peso ai tempi di preparazione
     private int maxMorningOrder;
     private int maxAfternoonOrder;
     private boolean enabled; // Gestisce il blocco degli ordini
