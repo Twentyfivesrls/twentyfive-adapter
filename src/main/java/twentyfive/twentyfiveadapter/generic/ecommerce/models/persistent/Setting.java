23114194fef652364fe44b1492e61a78af563d74
Raikozu <francescogaglianese93@gmail.com>
2024-04-24 13:18:03 +0200

    -modified settings

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/persistent/Setting.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/persistent/Setting.java
@@ -3,10 +3,13 @@ package twentyfive.twentyfiveadapter.generic.ecommerce.models.persistent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.util.Pair;
 import twentyfive.twentyfiveadapter.generic.ecommerce.utils.DateRange;
 import twentyfive.twentyfiveadapter.generic.ecommerce.utils.WeightRange;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -16,7 +19,7 @@ public class Setting {
     private String id;
     private DateRange businessHours; // Orari di apertura collegati alla lavorazione del prodotto
     private DateRange labHours;
-    private Map<WeightRange, LocalDateTime> preparationTimeRanges; // Mappa che collega range di peso ai tempi di preparazione
+    private Map<String, Map<Boolean, List<Pair<Double,LocalDateTime>>>> preparationTimeRanges; // Mappa che collega range di peso ai tempi di preparazione
     private int maxMorningOrder;
     private int maxAfternoonOrder;
     private boolean enabled; // Gestisce il blocco degli ordini
