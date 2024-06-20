b50a6ea92aaf065e318bfdec17599bc8b351c0b7
Raikozu <francescogaglianese93@gmail.com>
2024-06-20 12:47:19 +0200

    -Removed chocolateChips, now it's a customized key.

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/ProductInPurchase.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/ProductInPurchase.java
@@ -15,7 +15,6 @@ public class ProductInPurchase extends ItemInPurchase{
     private double weight; // Il peso del prodotto
     private String shape; // La forma del prodotto, potrebbe essere meglio come Enum se le forme sono predefinite
     private Map<String, String> customization; // Una mappa degli ingredienti personalizzati, dove la chiave è l'ID dell'ingrediente
-    private boolean chocolateChips; // Un booleano per indicare se ci sono gocce di cioccolato o meno; sostituire con il nome corretto se necessario
     private String attachment; // Un allegato, presumibilmente un URL a un'immagine o a un documento
     private LocalDate deliveryDate; // La data di consegna del prodotto
 
