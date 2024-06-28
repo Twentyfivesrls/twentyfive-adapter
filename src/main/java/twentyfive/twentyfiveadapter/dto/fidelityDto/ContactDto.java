21421b72a3bc64ea97440b77e90d6391e7d9017c
GiuseppeDominici <giuseppedominici99@gmail.com>
2024-06-28 14:05:22 +0200

    added contact dto fidelity

---
+++ src/main/java/twentyfive/twentyfiveadapter/dto/fidelityDto/ContactDto.java
@@ -0,0 +1,24 @@
+package twentyfive.twentyfiveadapter.dto.fidelityDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContactDto {
+
+    private String id;
+    private String ownerId;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String phoneNumber;
+    private Date creationDate;
+    private int associatedCards;
+    private int prizes;
+}
