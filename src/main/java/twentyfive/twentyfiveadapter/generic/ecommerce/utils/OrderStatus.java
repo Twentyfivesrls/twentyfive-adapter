ea2cb2eaf7ada5094c812bd1d4e8943c10e4e187
Raikozu <francescogaglianese93@gmail.com>
2024-07-03 14:33:47 +0200

    -new OrderStatus, modificato da Pasticceria

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/utils/OrderStatus.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/utils/OrderStatus.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum OrderStatus {
     RICEVUTO("Ricevuto"),
     IN_PREPARAZIONE("In preparazione"),
+    MODIFICATO_DA_PASTICCERIA("Modificato da pasticceria"),
     PRONTO("Pronto"),
     ANNULLATO("Annullato"),
     COMPLETO("Completo");
