93315b192dce87832c1e9d9252e4a826dfefbe3f
Raikozu <francescogaglianese93@gmail.com>
2024-09-02 12:54:09 +0200

    added Subscription and Date of the payment in UserPayment

--- src/main/java/twentyfive/twentyfiveadapter/models/subscriptionModels/UserPayment.java
+++ src/main/java/twentyfive/twentyfiveadapter/models/subscriptionModels/UserPayment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Document
@@ -16,5 +17,7 @@ public class UserPayment {
     @Id
     private String id;
     private String userId;
+    private LocalDateTime paymentDate; //Time when the payment is done
+    private Subscription subscription; //for what is the payment?
     private Map<String,Object> paymentData; // todo currently saved as Map because we don't know all the data about the payments
 }
