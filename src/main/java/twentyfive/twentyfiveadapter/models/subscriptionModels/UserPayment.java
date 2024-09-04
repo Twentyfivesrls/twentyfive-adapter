2e77841ea6a6cf6ffbce6cc52b88ad5e0944fb83
Raikozu <francescogaglianese93@gmail.com>
2024-09-04 16:03:57 +0200

    userPayment now is linked at UserSubscription and not at the single Subscription

--- src/main/java/twentyfive/twentyfiveadapter/models/subscriptionModels/UserPayment.java
+++ src/main/java/twentyfive/twentyfiveadapter/models/subscriptionModels/UserPayment.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import twentyfive.twentyfiveadapter.dto.subscriptionDto.UserSubscription;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -16,8 +17,7 @@ import java.util.Map;
 public class UserPayment {
     @Id
     private String id;
-    private String userId;
     private LocalDateTime paymentDate; //Time when the payment is done
-    private Subscription subscription; //for what is the payment?
+    private UserSubscription userSubscription; //for what is the payment?
     private Map<String,Object> paymentData; // todo currently saved as Map because we don't know all the data about the payments
 }
