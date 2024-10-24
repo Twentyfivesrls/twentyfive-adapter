971bbaf782645c08ab9490e6cbf20f4e514ff187
SimoneBria <simone.bria.e@soluzionimediacom.com>
2024-10-24 22:24:24 +0200

    new version of library adapter 0.0.227 - new version of model TicTicCounterShopper for tictic seller profile

--- src/main/java/twentyfive/twentyfiveadapter/models/tictickModels/TicTicShopper.java
+++ src/main/java/twentyfive/twentyfiveadapter/models/tictickModels/TicTicShopper.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "tictic_shopper")
-public class TicTicCounterShopper {
+public class TicTicShopper {
 
     @Id
     private String id;
