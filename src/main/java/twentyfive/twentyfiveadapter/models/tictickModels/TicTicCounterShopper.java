4e18ac15baa845d5ea09e5ddd5088692961723d4
SimoneBria <simone.bria.e@soluzionimediacom.com>
2024-10-24 22:05:03 +0200

    new version of library adapter 0.0.226 - new version of model TicTicCounterShopper for tictic seller profile

---
+++ src/main/java/twentyfive/twentyfiveadapter/models/tictickModels/TicTicCounterShopper.java
@@ -0,0 +1,23 @@
+package twentyfive.twentyfiveadapter.models.tictickModels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "tictic_shopper")
+public class TicTicCounterShopper {
+
+    @Id
+    private String id;
+    private String ownerId;
+    private Integer orderedPlates;
+    private Integer soldPlates;
+    private Integer remainingPlates;
+    private Integer customerCount;
+
+}
