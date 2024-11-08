bf1c1aa6a20a3924c3cfccca12500c9a5213cc2a
rebecca <rliguori96@gmail.com>
2024-11-08 18:59:20 +0100

    uploaded qrGen models

---
+++ src/main/java/twentyfive/twentyfiveadapter/models/qrGenModels/QrCodeGroup.java
@@ -0,0 +1,26 @@
+package twentyfive.twentyfiveadapter.models.qrGenModels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "tictic_qrcode_group")
+public class QrCodeGroup {
+    @Id
+    private String idQrCode;
+    private String nameQrCode;
+    private String groupName;
+    private String link;
+    private String type;
+    private String username;
+    private Boolean isActivated;
+    private String ownerId;
+    private String customerId;
+}
