1af78043f4f3213bcd6ba2c467468a227e6bd91b
Raikozu <francescogaglianese93@gmail.com>
2024-11-28 17:22:18 +0100

    -Added CouponHome all'interno Coupon

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/utils/Message.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/utils/Message.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CouponHome {
+public class Message {
     private String title;
     private String description;
     private String imgUrl;
