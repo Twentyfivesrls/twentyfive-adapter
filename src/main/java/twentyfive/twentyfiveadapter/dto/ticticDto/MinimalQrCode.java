6de0e4e58ae53093ebe84c446ddbb5935adf8a5e
Raikozu <francescogaglianese93@gmail.com>
2024-12-18 12:56:30 +0100

    -Some dtos for QrCode and TTAnimals

--- src/main/java/twentyfive/twentyfiveadapter/dto/ticticDto/MinimalQrCode.java
+++ src/main/java/twentyfive/twentyfiveadapter/dto/ticticDto/MinimalQrCode.java
@@ -1,5 +1,12 @@
 package twentyfive.twentyfiveadapter.dto.ticticDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MinimalQrCode {
     private String id;
     private String imgUrl;
--- src/main/java/twentyfive/twentyfiveadapter/dto/ticticDto/TTAnimalAndQrCode.java
+++ src/main/java/twentyfive/twentyfiveadapter/dto/ticticDto/TTAnimalAndQrCode.java
@@ -1,7 +1,13 @@
 package twentyfive.twentyfiveadapter.dto.ticticDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import twentyfive.twentyfiveadapter.models.tictickModels.TTAnimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TTAnimalAndQrCode {
     private TTAnimal animal;
     private MinimalQrCode qrCode;
