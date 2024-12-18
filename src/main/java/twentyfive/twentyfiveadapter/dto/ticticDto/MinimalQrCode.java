9bf45cf50fa33ab5627d03e3b23cbb9bc8ce9c85
Raikozu <francescogaglianese93@gmail.com>
2024-12-18 12:48:07 +0100

    -Some dtos for QrCode and TTAnimals

---
+++ src/main/java/twentyfive/twentyfiveadapter/dto/ticticDto/MinimalQrCode.java
@@ -0,0 +1,6 @@
+package twentyfive.twentyfiveadapter.dto.ticticDto;
+
+public class MinimalQrCode {
+    private String id;
+    private String imgUrl;
+}
---
+++ src/main/java/twentyfive/twentyfiveadapter/dto/ticticDto/TTAnimalAndQrCode.java
@@ -0,0 +1,8 @@
+package twentyfive.twentyfiveadapter.dto.ticticDto;
+
+import twentyfive.twentyfiveadapter.models.tictickModels.TTAnimal;
+
+public class TTAnimalAndQrCode {
+    private TTAnimal animal;
+    private MinimalQrCode qrCode;
+}
