527f1e40c16a0de3443e68ae2c5b88fed663ab5b
Sgranfrix <marco.rija@gmail.com>
2024-12-19 17:19:54 +0100

    Modifiche alla struttura di base dei collegamenti tra le classi Subscription, UserSubscription e UserPayment

--- src/main/java/twentyfive/twentyfiveadapter/dto/subscriptionDto/MinimalSubscription.java
+++ src/main/java/twentyfive/twentyfiveadapter/dto/subscriptionDto/MinimalSubscription.java
@@ -11,10 +11,20 @@ import java.util.List;
 @AllArgsConstructor
 public class MinimalSubscription {
 
+    private String id;
     private String name;
     private List<String> appIds;
     private double price;
     private Frequency frequency;
     private Discount discount;
 
+    public String getType(){
+        if(this.appIds.size()>1){
+            return "Pack";
+        } else if (this.appIds.size()==1){
+            return "Single";
+        } else {
+            return "No app linked";
+        }
+    }
 }
