47339c11529764d66d0ef5ef8cff2ec8c60ccba6
Sgranfrix <marco.rija@gmail.com>
2025-01-09 17:01:14 +0100

    Modifiche allo UserPayment

--- src/main/java/twentyfive/twentyfiveadapter/models/subscriptionModels/UserPayment.java
+++ src/main/java/twentyfive/twentyfiveadapter/models/subscriptionModels/UserPayment.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import twentyfive.twentyfiveadapter.dto.subscriptionDto.Price;
 import twentyfive.twentyfiveadapter.dto.subscriptionDto.UserSubscription;
 
 import java.time.LocalDateTime;
@@ -18,5 +19,7 @@ public class UserPayment {
     @Id
     private String id;
     private LocalDateTime paymentDate; //Time when the payment is done
+    private Price price;
+    private String paymentMethod;
     private Map<String,Object> paymentData; // todo currently saved as Map because we don't know all the data about the payments
 }
