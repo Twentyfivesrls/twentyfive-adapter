e7a842c3586e602f820007cbacc1d20e690aa0e5
Raikozu <francescogaglianese93@gmail.com>
2025-02-06 16:40:57 +0100

    Aggiornato Enum frequency con traduzioni

--- src/main/java/twentyfive/twentyfiveadapter/dto/subscriptionDto/Frequency.java
+++ src/main/java/twentyfive/twentyfiveadapter/dto/subscriptionDto/Frequency.java
@@ -6,12 +6,24 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Frequency {
-    DAILY(1, 0),
-    WEEKLY(7, 0),
-    MONTHLY(0, 1),
-    QUARTERLY(0, 3),
-    ANNUAL(0, 12);
+    DAILY(1, 0, "Giornaliera"),
+    WEEKLY(7, 0, "Settimanale"),
+    MONTHLY(0, 1, "Mensile"),
+    QUARTERLY(0, 3, "Trimestrale"),
+    ANNUAL(0, 12, "Annuale");
 
     private final int days;
     private final int months;
+    private final String translate;
+
+    public static Frequency getFrequency(String translate) {
+        for (Frequency frequency : Frequency.values()) {
+            if (frequency.getTranslate().equals(translate)) {
+                return frequency;
+            }
+        }
+        return null;
+    }
+
+
 }
