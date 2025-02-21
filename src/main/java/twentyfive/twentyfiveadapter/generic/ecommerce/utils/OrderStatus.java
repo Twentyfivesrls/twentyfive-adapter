e1edf3f39e501c9f36f3ea694f9c07f32d989295
Raikozu <francescogaglianese93@gmail.com>
2025-02-21 11:22:03 +0100

    Added new Status in OrderStatus

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/utils/OrderStatus.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/utils/OrderStatus.java
@@ -1,12 +1,17 @@
 package twentyfive.twentyfiveadapter.generic.ecommerce.utils;
 
+import com.paypal.orders.PaymentMethod;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public enum OrderStatus {
     RICEVUTO("Ricevuto"),
+    IN_PASTICCERIA("In pasticceria"),
     IN_PREPARAZIONE("In preparazione"),
     MODIFICATO_DA_PASTICCERIA("Modificato da pasticceria"),
     PRONTO("Pronto"),
@@ -17,5 +22,20 @@ public enum OrderStatus {
 
     public static OrderStatus[] getStatusEditable(){
         return new OrderStatus[] {COMPLETO,ANNULLATO};
+    }
+
+    public static OrderStatus fromValue(String value) {
+        for (OrderStatus status : OrderStatus.values()) {
+            if (status.getStatus().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        return null;
+    }
+
+    public static List<String> getValues() {
+        return Arrays.stream(OrderStatus.values())
+                .map(OrderStatus::getStatus)
+                .toList();
     }
 }
