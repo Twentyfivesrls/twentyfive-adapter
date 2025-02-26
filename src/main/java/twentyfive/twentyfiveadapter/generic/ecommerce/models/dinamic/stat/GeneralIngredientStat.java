069c6a55b45473d4c9a905b6cc1b3955c3fa0369
Raikozu <francescogaglianese93@gmail.com>
2025-02-26 16:27:12 +0100

    -fixed Stats for ecommerce.

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/stat/GeneralIngredientStat.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/stat/GeneralIngredientStat.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class GeneralIngredientStat {
-    private long usedIngredients;
-    private long totalIngredients;
-    private long totalUsedIngredients;
+    private List<String> distinctUsedIngredients; //Id ingredienti usati diversi
+    private long totalIngredients; //Tutti gli ingredienti presenti al giorno in cui si è fatta la statistica
+    private long totalUsedIngredients; //Totale ingredienti usati
 }
--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/stat/GeneralProductStat.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/dinamic/stat/GeneralProductStat.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GeneralProductStat {
     private long totalProductsSold; // Numero totale di prodotti venduti
     private long totalOrders; // Numero totale di ordini effettuati
-    private long totalCustomersServed; // Numero totale di clienti serviti
+    private List<String> distinctCustomerServed; // Id clienti diversi serviti
     private double totalRevenue; // Totale guadagnato
 }
