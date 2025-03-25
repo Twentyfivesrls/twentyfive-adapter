90df6635ddbcd5f0746936153920421f476901ab
Raikozu <francescogaglianese93@gmail.com>
2025-03-25 15:18:05 +0100

    Added LoginRequest for our apis for ms-user-business

---
+++ src/main/java/twentyfive/twentyfiveadapter/dto/keycloakDto/LoginRequest.java
@@ -0,0 +1,13 @@
+package twentyfive.twentyfiveadapter.dto.keycloakDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
