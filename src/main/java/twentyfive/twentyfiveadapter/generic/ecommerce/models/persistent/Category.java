b55ccf238be874207fe9a8e3dded55fb128bb9f1
Raikozu <francescogaglianese93@gmail.com>
2025-07-29 12:02:05 +0200

    added equals and hashCode for category based on ID.

--- src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/persistent/Category.java
+++ src/main/java/twentyfive/twentyfiveadapter/generic/ecommerce/models/persistent/Category.java
@@ -3,13 +3,16 @@ package twentyfive.twentyfiveadapter.generic.ecommerce.models.persistent;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
+    @EqualsAndHashCode.Include
     private String id;
     private String type;
     private String name;
